0e2e0b9bced8d4c03a297aa10af66ef9efe97a53
Avrodip Saha <avrodipsaha1111@gmail.com>
2022-06-25 03:04:19 +0530

    add

--- src/Componect/Display.js
+++ src/Componect/Display.js
@@ -1,67 +1,36 @@
 import React, { useEffect, useState } from "react";
+import Stopwatch from "./Stopwatch";
 
-// to get data
-const getLocalItem = () => {
-  let best = localStorage.getItem("bestTime");
-
-  if (best) {
-    return JSON.parse(localStorage.getItem("bestTime"));
-  } else {
-    return [];
-  }
-};
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 const Display = () => {
   const [data, setData] = useState("");
-  // const [count, setCount] = useState(0);
-  const [time, setTime] = useState(0.0);
-  const [score, setScore] = useState(getLocalItem);
-
-  if (time < score) {
-    if (!score) {
-      localStorage.setItem("bestTime", time);
-    } else {
-      localStorage.removeItem();
-      localStorage.setItem("bestTime", time);
-    }
-  }
-
-  //add data to localStorage
-  useEffect(() => {
-    localStorage.setItem("bestTime", JSON.stringify(score));
-  }, [score]);
-
-  console.log(score);
-
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+  const [rndData, setRndData] = useState("");
+  const [keyval, setKeyval] = useState("");
 
   const Alphabet = "ABCDEFGHIJKLMNOPQURSTUVWXYZ";
-  let random = getRandomInt(1, 20);
-  let rndAlfa = Alphabet[random];
 
-  // const detectKeyDown = (e) => {
-  //   setData(e.key);
-  // };
-  // useEffect(() => {
-  //   document.addEventListener("keydown", detectKeyDown, true);
-  //   getRandomInt(1.2);
-  // }, []);
+  useEffect(() => {
+    let random = getRandomInt(1, 20);
+    setRndData(Alphabet[random]);
 
-  // const handleKeyPress = (event) => {
-  //   event.preventDefalt();
-  //   if (event.key === rndAlfa) {
-  //     console.log("enter press here! ");
-  //   }
-  // };
+    if (rndData === keyval) {
+      setRndData(Alphabet[random]);
+    }
+  }, [keyval]);
 
   const handleChange = (event) => {
     setData(event.target.value);
-    console.log(data);
   };
+
+  const handelKeyDown = (e) => {
+    setKeyval(e.key);
+  };
+
   return (
     <>
       <div className="header">
@@ -70,25 +39,26 @@ const Display = () => {
           Typing game to see how fast you type. Timer starts when you do :)
         </p>
       </div>
+
       <div className="body">
         <div className="container">
-          <div className="content">{rndAlfa}</div>
+          <div className="content">{rndData}</div>
         </div>
       </div>
+
       <div className="footer">
         <div className="timer">
-          <b> Time: {time.toFixed(3)}s </b>
+          <b> Time: 0.000s </b>
         </div>
-        <div className="hi_score">my best time:{score}s!</div>
+        <div className="hi_score">my best time:00.21s!</div>
         <form>
           <label>
             <input
               type="text"
               placeholder="Type here"
-              autoFocus
               className="input"
               value={data}
-              // onKeyPress={handleKeyPress}
+              onKeyDown={handelKeyDown}
               onChange={handleChange}
             />
             <div className="reset" onClick={() => setData("")}>
@@ -96,6 +66,7 @@ const Display = () => {
             </div>
           </label>
         </form>
+        <Stopwatch />
       </div>
     </>
   );
