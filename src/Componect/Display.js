75df25a165dca95dcc5d8baac68aa662559dfbda
Avrodip Saha <avrodipsaha1111@gmail.com>
2022-06-25 13:54:06 +0530

    adding project

--- src/Componect/Display.js
+++ src/Componect/Display.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Stopwatch from "./Stopwatch";
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -9,26 +8,48 @@ function getRandomInt(min, max) {
 
 const Display = () => {
   const [data, setData] = useState("");
-  const [rndData, setRndData] = useState("");
+  const [rndData, setRndData] = useState("A");
   const [keyval, setKeyval] = useState("");
-
+  const [show, setShow] = useState("");
+  const [score, setScore] = useState(Number.MAX_VALUE);
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
   const Alphabet = "ABCDEFGHIJKLMNOPQURSTUVWXYZ";
 
   useEffect(() => {
-    let random = getRandomInt(1, 20);
-    setRndData(Alphabet[random]);
-
-    if (rndData === keyval) {
-      setRndData(Alphabet[random]);
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
     }
-  }, [keyval]);
+    return () => clearInterval(interval);
+  }, [running]);
 
   const handleChange = (event) => {
     setData(event.target.value);
+    if (data.length === 20) {
+      setRunning(false);
+      setTime(0);
+      if (time < score) {
+        setScore(time);
+        setShow("Success");
+      } else {
+        setShow("Failure");
+      }
+    }
   };
 
   const handelKeyDown = (e) => {
     setKeyval(e.key);
+    let random = getRandomInt(1, 20);
+    if (rndData.toString() === keyval.toString()) {
+      setRndData(Alphabet[random]);
+    }
+
+    setRunning(true);
   };
 
   return (
@@ -42,31 +63,48 @@ const Display = () => {
 
       <div className="body">
         <div className="container">
-          <div className="content">{rndData}</div>
+          <div className="content">{data.length > 20 ? show : rndData}</div>
         </div>
       </div>
 
       <div className="footer">
         <div className="timer">
-          <b> Time: 0.000s </b>
+          <b>Time:</b>
+          <span>
+            {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
+            {("0" + ((time / 10) % 100)).slice(-3)}s
+          </span>
         </div>
-        <div className="hi_score">my best time:00.21s!</div>
+        <div className="hi_score">
+          my best time:
+          <span>
+            {("0" + Math.floor((score / 1000) % 60)).slice(-2)}.
+            {("0" + ((score / 10) % 100)).slice(-2)}s!
+          </span>
+        </div>
         <form>
           <label>
             <input
               type="text"
               placeholder="Type here"
               className="input"
+              disabled={data.length <= 20 ? false : true}
               value={data}
-              onKeyDown={handelKeyDown}
+              onKeyPress={handelKeyDown}
               onChange={handleChange}
             />
-            <div className="reset" onClick={() => setData("")}>
+            <div
+              className="reset"
+              onClick={() => {
+                setData("");
+                setTime(0);
+                setRunning(false);
+              }}
+            >
               Reset
             </div>
           </label>
         </form>
-        <Stopwatch />
       </div>
     </>
   );
